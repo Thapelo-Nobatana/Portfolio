76a591c59e37087668f775cd33494c9193eb08f1
Karabo Nobatana <nobatanathapelo@gmail.com>
2025-05-28 08:36:11 +0200

    developed hero section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+// import React from "react";
 import Hero from "../../Components/Sections/Hero";
+import Skills from "../../Components/Sections/Skills";
+import Experience from "../../Components/Sections/Experience";
+import AboutMe from "../../Components/Sections/AboutMe";
 
 const Home = () => {
   return (
@@ -7,9 +10,15 @@ const Home = () => {
       <section className="hero">
         <Hero />
       </section>
-      <section className="hero"></section>
-      <section className="hero"></section>
-      <section className="hero"></section>
+      <section className="skills">
+        <Skills />
+      </section>
+      <section className="experience">
+        <Experience />
+      </section>
+      <section className="about-me">
+        <AboutMe />
+      </section>
     </>
   );
 };
