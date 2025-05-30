acda799ae4f9fd658814cedfc9a1185a2fbfbff8
Thapelo Nobatana <nobatanathapelo@gmail.com>
2025-05-30 10:09:23 +0200

    new changes

--- src/Components/Sections/Projects.jsx
+++ src/Components/Sections/Projects.jsx
@@ -1,4 +1,6 @@
 // import React from 'react'
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import "./Projects.css";
 
 const Projects = () => {
@@ -7,12 +9,33 @@ const Projects = () => {
       <div className="container">
         <div className="content">
           <div className="top-heading">
-            <h2></h2>
+            <h2>
+              my <strong>Projects</strong>
+            </h2>
           </div>
           <div className="projects-wrapper">
             <div className="project-wrapper">
-              <img src="" alt="" />
-              <div className="heading-wrapper"></div>
+              <img src="" alt="project-image" />
+              <div className="heading-wrapper">
+                <h1>01</h1>
+                <h2>BiteSwift Food Delivery App</h2>
+                <p>
+                  BiteSwift is a modern food delivery app that connects users
+                  with their favorite restaurants for quick, easy, and seamless
+                  food ordering. Built with React, Node.js, Express, and
+                  MongoDB, the app ensures a smooth user experience with
+                  real-time tracking, secure payments, and personalized
+                  recommendations.
+                </p>
+                <div className="git-wrapper">
+                  <FaExternalLinkAlt />
+
+                  <a>
+                    {" "}
+                    git <FaGithub color="#ffffff" />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
