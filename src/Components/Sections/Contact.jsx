cd9972bd865afe8c8e198c9cb79ca7552ad1d854
Thapelo Nobatana <nobatanathapelo@gmail.com>
2025-06-01 15:35:35 +0200

    updated changes

--- src/Components/Sections/Contact.jsx
+++ src/Components/Sections/Contact.jsx
@@ -1,4 +1,4 @@
-// import axios from "axios"
+import axios from "axios";
 import "./Contact.css";
 import { useState } from "react";
 
@@ -37,7 +37,7 @@ const Contact = () => {
       setError("There was an error submitting the form. Please try again.");
     } finally {
       setLoading(false);
-      setTimeout(() => setDrop(false), 500);
+      setTimeout(() => setDrop(false), 600);
     }
   };
 
@@ -57,11 +57,33 @@ const Contact = () => {
             ></textarea>
             {error && <p className="error-message">{error}</p>}
             <button type="submit" disabled={loading}>
-              {loading ? "Submitting..." : "Submit"}
+              {loading ? "Submitting..." : "Get In Touch"}
             </button>
           </form>
 
-          <div className="text-content"></div>
+          <div className="text-content">
+            <h2>
+              Let's <strong>talk</strong> about something special!
+            </h2>
+            <p>
+              Creativity has always been at the heart of my professional
+              journey, and I am deeply driven by the challenge of pushing
+              boundaries in the digital design space. My passion lies in
+              crafting interactive experiences that not only capture attention
+              but also leave a lasting impression. I strive to design solutions
+              that are not just aesthetically compelling but also intuitive and
+              meaningful for the user. Each project I undertake is an
+              opportunity to blend form and function in a way that enhances
+              usability while also evoking a sense of wonder and engagement.
+              What fuels my enthusiasm for design is the potential to connect
+              with people through thoughtful, immersive experiences. Whether
+              it's a web platform, mobile app, or digital installation, I
+              believe in the power of design to transform how users interact
+              with technology.
+            </p>
+            <a href="http://">nobatanathapelo@gmail.com</a>
+            <p>Call: 081 514 6476</p>
+          </div>
         </div>
       </div>
     </div>
